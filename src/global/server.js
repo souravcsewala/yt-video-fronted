4ac13537eeed27532e87b8a68776cb21377440ff
souravcsewala <souravssc077@gmail.com>
2025-03-14 12:09:46 +0530

    update

--- src/global/server.js
+++ src/global/server.js
@@ -1,4 +1,4 @@
 
      // export const serverUrl="http://localhost:7050/sourav/yt-video-download/app"
 
-      export const serverUrl="https://broad-universe-booth-configure.trycloudflare.com/sourav/yt-video-download/app"
+      export const serverUrl="https://outlined-peter-pub-mag.trycloudflare.com/sourav/yt-video-download/app"
